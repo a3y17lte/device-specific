197de6de930b4eab6c48c4f6247b60a8890ffa91
DarkLord1731 <darklord1731@gmail.com>
2018-02-04 19:31:53 +0530

    on7xelte: Build wpa supplicant HIDL

--- board/wifi.mk
+++ board/wifi.mk
@@ -26,6 +26,5 @@ WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcmdhd_apsta.bin"
 WIFI_BAND                        := 802_11_ABG
-
-# Wifi Macloader
-BOARD_HAVE_SAMSUNG_WIFI := true
+WPA_SUPPLICANT_USE_HIDL          := true
+BOARD_HAVE_SAMSUNG_WIFI          := true
