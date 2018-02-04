52fb9dbbf6e7ea146247b1d762e12bb2919ffcfb
DarkLord1731 <darklord1731@gmail.com>
2018-02-04 18:51:26 +0530

    on7xelte: Build unified audio HAL

--- product/audio.mk
+++ product/audio.mk
@@ -17,4 +17,4 @@
 # Audio Configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
