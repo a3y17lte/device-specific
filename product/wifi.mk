8cba139d99dffb1cdbb5befc5ef80f2fc896d121
DarkLord1731 <darklord1731@gmail.com>
2018-02-04 18:20:48 +0530

    on7xelte: Add Wi-Fi supplicant configs from stock

--- product/wifi.mk
+++ product/wifi.mk
@@ -22,3 +22,8 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     wpa_supplicant \
     wpa_supplicant.conf
+
+# Wf-Fi Configs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
