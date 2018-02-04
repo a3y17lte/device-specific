ac5c73412295164f3d1e2c7f2cb86d0edc9a60aa
DarkLord1731 <darklord1731@gmail.com>
2018-02-04 20:23:38 +0530

    on7xelte: Add vendor blob extraction scripts from vendor/cm
    Command: . extract-files.sh path/to/system

--- extract-files.sh
+++ extract-files.sh
@@ -1,39 +1,62 @@
 #!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-VENDOR=samsung
+set -e
+
 DEVICE=on7xelte
+VENDOR=samsung
 
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
-    OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
-    FILE=${PARSING_ARRAY[0]}
-    DEST=${PARSING_ARRAY[1]}
-    if [ -z $DEST ]
-    then
-        DEST=$FILE
-    fi
-    DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-        mkdir -p $BASE/$DIR
-    fi
+CM_ROOT="$MY_DIR"/../../..
 
-    if [ -z "$STOCK_ROM_DIR" ]; then
-        adb pull /system/$FILE $BASE/$DEST
-    else
-        cp $STOCK_ROM_DIR/$FILE $BASE/$DEST
-    fi
+HELPER=extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+fi
+. "$HELPER"
 
-    # if file does not exist try destination
-    if [ "$?" != "0" ]
-    then
-        if [ -z "$STOCK_ROM_DIR" ]; then
-            adb pull /system/$DEST $BASE/$DEST
-        else
-            cp $STOCK_ROM_DIR/$DEST $BASE/$DEST
-        fi
-    fi
+# Default to sanitizing the vendor folder before extraction
+CLEAN_VENDOR=true
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -n | --no-cleanup )     CLEAN_VENDOR=false
+                                ;;
+        -s | --section )        shift
+                                SECTION=$1
+                                CLEAN_VENDOR=false
+                                ;;
+        * )                     SRC=$1
+                                ;;
+    esac
+    shift
 done
 
-./setup-makefiles.sh
+if [ -z "$SRC" ]; then
+    SRC=adb
+fi
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" false "$CLEAN_VENDOR"
+
+extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
+
+"$MY_DIR"/setup-makefiles.sh
