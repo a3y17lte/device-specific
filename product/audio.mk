225da0f5bde0f02c2293d605e7640aa99b311f67
DarkLord1731 <darklord1731@gmail.com>
2018-02-11 19:28:09 +0530

    on7xelte: Move mixer_paths to /vendor

--- product/audio.mk
+++ product/audio.mk
@@ -17,4 +17,4 @@
 # Audio Configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
+    $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/vendor/etc/mixer_paths_0.xml
