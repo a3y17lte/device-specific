dc433d48710fa7f5cc82bc8669af676011a4e88c
Matt Grf <31405855+McFy49@users.noreply.github.com>
2018-02-28 19:07:31 +0100

    Up to new defconfig name

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,4 +16,4 @@
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/exynos7870
-TARGET_KERNEL_CONFIG := lineage-a3y17lte_defconfig
+TARGET_KERNEL_CONFIG := lineage_a3y17lte-defconfig
