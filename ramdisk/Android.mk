0b9598af2543a6d3070d9b8b8f75ef6a44a9dd03
McFy49 <g.matthieu49@gmail.com>
2018-03-03 13:06:28 +0100

    added init.target.rc *Added perm *specific line from a3y17lte stock nougat firmware

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -21,3 +21,11 @@ LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := init.wifi.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.target.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := BOOT
+LOCAL_SRC_FILES    := init.target.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
