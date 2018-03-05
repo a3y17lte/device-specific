95194d96bc96c97bdcaa8777de2fe4acba200356
Matt Grf <31405855+McFy49@users.noreply.github.com>
2018-03-05 11:18:44 +0100

    remove audioFX moved to audio.mk

--- product/audio.mk
+++ product/audio.mk
@@ -20,3 +20,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_gains.xml:system/etc/mixer_gains.xml \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
     $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
+
+PRODUCT_PACKAGES += \
+    RemovePackages
