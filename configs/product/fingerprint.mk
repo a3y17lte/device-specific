18f17e1ec13f5df229e3e89fd4ff7bbfbd9f1b50
Matt Grf <g.matthieu49@gmail.com>
2018-04-16 14:44:42 +0200

    a3y17lte: Build hall correctly

--- configs/product/fingerprint.mk
+++ configs/product/fingerprint.mk
@@ -16,6 +16,8 @@
 
 # Fingerprint
 PRODUCT_PACKAGES += \
+    mcDriverDaemon \
     fingerprint.exynos5 \
     libbauthtzcommon_shim \
+    android.hardware.biometrics.fingerprint@2.1-impl \
     android.hardware.biometrics.fingerprint@2.1-service
